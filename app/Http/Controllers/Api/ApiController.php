2ff00420a56947416d096267cf9f77f6e926b12d
veysiygt <veysiygt21@gmail.com>
2023-03-18 23:51:48 +0300

    sendResponse and sendError have been completed

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    public function sendResponse($result, $message){
+        
+        $response = [
+            'data' => $result,
+            'success' => true,
+            'message' => $message
+        ];
+
+        return response()->json($response,200);
+        
+    }
+
+    public function sendError($error, $errorMessage = []){
+        
+        $response = [
+            'data' => null,
+            'success' => false,
+            'message' => $error
+        ];
+
+        if(!empty($errorMessages)){
+            $response['message'] = $errorMessage;
+        }
+
+        return response()->json($response);
+        
+    } 
+
+}
